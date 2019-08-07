fc60ea96908ad001194b94d01ee1224d8501ad7f
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-08-07 05:13:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { useEffect } from 'react';
 import { loadUsers } from './actions';
 import { connect } from 'react-redux';
-import store from './store';
 import './App.css';
 import Card from './components/list';
 
 const App = (props) => {
 
-    const [loading, setLoading] = useState(false);
-
     useEffect(() => {
-            setLoading(true);
             props.loadUsers();
-            setLoading(false);
     }, []);
   return (
     <div className="App">
@@ -23,11 +18,5 @@ const App = (props) => {
     </div>
   );
 };
-
-
-const mapStateToProps = state => ({
-    users: state.users,
-    pages: state.pages
-});
 
 export default connect(null, { loadUsers })(App);
